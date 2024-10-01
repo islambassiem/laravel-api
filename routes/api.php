55301f925402897cb56ceb455778ddee278f0d3c
Islam <islambassiem@gmail.com>
2024-10-01 13:03:14 +0300

    lesson-3-custom-error-messages

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('list/categories', [CategoryController::class, 'list']);
+
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
